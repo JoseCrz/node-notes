6bc86486a1f045278bd1840f39314e4d501c663a
José Cuevas <josecueram@gmail.com>
2019-03-13 00:23:17 -0600

    Added yargs & implemented basic configuration for yargs commands

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 
-const command = process.argv[2]
+//---------<Configure yargs commands>----------
 
-if (command === 'new') {
-    console.log('Creating new note...')
-} else if (command === 'remove') {
-    console.log('Removing note...')
-} else {
-    console.log(chalk.bgRed('Not a valid command.'))
-}
+yargs.command({
+    command: 'new',
+    describe: 'Creates a new note.',
+    handler: function () {
+        console.log(chalk.yellow('Creating a new note...'))
+    }
+})
 
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note',
+    handler: function () {
+        console.log(chalk.yellow('Removing a note...'))
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+        console.log(chalk.yellow('Listing notes..'))
+    }
+})
+
+yargs.command({
+    command: 'open',
+    describe: 'Opens up a note',
+    handler: function () {
+        console.log(chalk.yellow('Opening note...'))
+    }
+})
+
+console.log(yargs.argv)
 
